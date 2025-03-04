40648f920f2def0258ed6cd160fbe07945ec6691
collinslindaw93663 <collins_lindaw93663@gmx.com>
2025-03-04 14:33:05 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,9 @@
+int main() {
+    // Generate a random number between 1 and 10
+    int x = rand() % 10 + 1;
+    
+    // Print the result
+    cout << "Random number: " << x << endl;
+    
+    return 0;
+}
