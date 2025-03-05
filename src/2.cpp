fc76512102b1e4090e0097fe8d7b941fb248b2b4
collinslindaw93663 <collins_lindaw93663@gmx.com>
2025-03-05 15:21:20 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  int x = 5;
+  int y = 10;
+  int z = x + y;
+  cout << "The sum of x and y is: " << z << endl;
+  return 0;
+}
